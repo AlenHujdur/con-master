05bd8ebf971c468810ba886657883cd63de71006
AlenHujdur <hujduralen@gmail.com>
2018-03-03 17:17:17 +0100

    pp

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,18 +36,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Derived via Base reference:");
+            /*Bazna i derivatna + virtual */
+            //Console.WriteLine("Derived via Base reference:");
 
-            Base b = new Derived();
-            b.DoSomethingVirtual();
-            b.DoSomethingNonVirtual();
+            //Base b = new Derived();
+            //b.DoSomethingVirtual();
+            //b.DoSomethingNonVirtual();
 
-            Console.WriteLine();
-            Console.WriteLine("Derived via Derived reference:");
+            //Console.WriteLine();
+            //Console.WriteLine("Derived via Derived reference:");
 
-            Derived d = new Derived();
-            d.DoSomethingVirtual();
-            d.DoSomethingNonVirtual();
+            //Derived d = new Derived();
+            //d.DoSomethingVirtual();
+            //d.DoSomethingNonVirtual();
+
+            /*Malo Linq*/
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            Console.WriteLine("Prosjek: " + list.Average().ToString());
+
+            string path = @"C:\";
+            foreach (string f in Directory.EnumerateDirectories(path))
+            { Console.WriteLine(f); }
+
 
         }
     }
