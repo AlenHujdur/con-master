ce3309ce5c63f9aa41cbc5d310c53aa15a3fd4cd
AlenHujdur <hujduralen@gmail.com>
2018-03-03 17:17:02 +0100

    vjez

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Base
+    {
+        public virtual void DoSomethingVirtual()
+        {
+            Console.WriteLine("Base.DoSomethingVirtual");
+        }
+
+        public void DoSomethingNonVirtual()
+        {
+            Console.WriteLine("Base.DoSomethingNonVirtual");
+        }
+    }
+
+    class Derived : Base
+    {
+        public override void DoSomethingVirtual()
+        {
+            Console.WriteLine("Derived.DoSomethingVirtual");
+        }
+
+        public new void DoSomethingNonVirtual()
+        {
+            Console.WriteLine("Derived.DoSomethingNonVirtual");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Derived via Base reference:");
+
+            Base b = new Derived();
+            b.DoSomethingVirtual();
+            b.DoSomethingNonVirtual();
+
+            Console.WriteLine();
+            Console.WriteLine("Derived via Derived reference:");
+
+            Derived d = new Derived();
+            d.DoSomethingVirtual();
+            d.DoSomethingNonVirtual();
+
+        }
+    }
+}
